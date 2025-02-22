a5c4f33f959411dd63709b6760089d40341548a0
Pablo-fp <pablofp1988@hotmail.com>
2025-02-22 13:32:56 +0100

    8.8: Authors view

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,25 +1,54 @@
-const Authors = (props) => {
-  const authors = [];
+import { useState, useEffect } from 'react';
+
+const Authors = () => {
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `
+          query {
+            allAuthors {
+              name
+              born
+              bookCount
+            }
+          }
+        `
+      })
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setAuthors(result.data.allAuthors);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div>
       <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      {authors.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <table>
+          <tbody>
+            <tr>
+              <th>name</th>
+              <th>born</th>
+              <th>books</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {authors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born || 'N/A'}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
